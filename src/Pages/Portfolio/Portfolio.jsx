f76fe0d5027e2e7fb5e69499373f18fe48c7d042
vasudev290 <vasukesavulu@gmail.com>
2024-08-06 13:49:29 +0530

    test5

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,96 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { FaRegEye } from 'react-icons/fa';
 
-
-const json = [
-    {
-        "id": 1,
-        "title": "Tiger Gym",
-        "category": "Frontend",
-        "image": "public/images/project-1.png",
-        "imageUrl": "https://tigerfitness.vercel.app/"
-    },
-    {
-        "id": 2,
-        "title": "Fresh Organic",
-        "category": "Frontend",
-        "image": "public/images/project-2.png",
-        "imageUrl": "https://vd-organic-grocery.vercel.app/"
-    },
-    {
-        "id": 3,
-        "title": "Travles",
-        "category": "Frontend",
-        "image": "public/images/project-3.png",
-        "imageUrl": "https://vd-travelsweb.vercel.app/"
-    },
-    {
-        "id": 4,
-        "title": "Stomy App",
-        "category": "Frontend",
-        "image": "public/images/project-4.png",
-        "imageUrl": "https://stromyapp.vercel.app/"
-    },
-    {
-        "id": 5,
-        "title": "TODO App.",
-        "category": "Frontend",
-        "image": "public/images/project-5.png",
-        "imageUrl": "https://todoapp-one-henna.vercel.app/"
-    },
-    {
-        "id": 6,
-        "title": "Timepiece",
-        "category": "Frontend",
-        "image": "public/images/project-6.png",
-        "imageUrl": "https://timepiece-iota.vercel.app/"
-    },
-    {
-        "id": 7,
-        "title": "CRUD Operation",
-        "category": "Frontend + React",
-        "image": "public/images/project-7.png",
-        "imageUrl": "https://crud-app-ashen.vercel.app/"
-    },
-    {
-        "id": 8,
-        "title": "TIC TOC TOE",
-        "category": "Frontend",
-        "image": "public/images/project-8.png",
-        "imageUrl": "https://tic-tac-toe-ivory-delta.vercel.app/"
-    },
-    {
-        "id": 9,
-        "title": "CARA E-com ",
-        "category": "Frontend",
-        "image": "public/images/project-9.png",
-        "imageUrl": "https://cara-e-com-website.vercel.app/"
-    },
-    {
-        "id": 10,
-        "title": ".EATFOODS ",
-        "category": "Frontend + React",
-        "image": "public/images/project-10.png",
-        "imageUrl": "https://eatfoods.vercel.app/"
-    }
-  ]
-
 const Portfolio = () => {
-
-
-    const Obj = JSON.stringify(json);
-    console.log("data", Obj)
-    const [projects, setprojects] = useState(Obj);
+    const [projects, setprojects] = useState([]);
     const [filteredProjects, setFilteredProjects] = useState([])
-    const [selectedCategory, setSelectedCategory] = useState("All");
+    const [selectedCategory, setSelectedCategory] = useState("All")
 
     useEffect(() => {
         const url = 'public/projects.json';
-        console.log("profilo", url)
+        console.log("profilo",url)
         fetch(url).then(res => res.json()).then(data => {
-            // setprojects(data)
+            setprojects(data)
             console.log(data);
-
+            
             setFilteredProjects(data)
         })
     }, [])
