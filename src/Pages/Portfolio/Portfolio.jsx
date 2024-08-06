3708d65cd514b6c950e5416de26e58f5cbd71f12
vasudev290 <vasukesavulu@gmail.com>
2024-08-06 12:27:50 +0530

    tst

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { FaRegEye } from 'react-icons/fa';
 
 const Portfolio = () => {
-    const [projects, setprojects]= useState([]);
-    const [filteredProjects, setFilteredProjects] =useState([])
+    const [projects, setprojects] = useState([]);
+    const [filteredProjects, setFilteredProjects] = useState([])
     const [selectedCategory, setSelectedCategory] = useState("All")
 
-    useEffect(()=> {
-        fetch('public/projects.json').then(res => res.json()).then(data => {
+    useEffect(() => {
+        const url = 'public/projects.json'
+        fetch(url).then(res => res.json()).then(data => {
             setprojects(data)
             setFilteredProjects(data)
         })
-    },[])
+    }, [])
 
-    const handleFilterClick= (category)=> {
+    const handleFilterClick = (category) => {
         setSelectedCategory(category);
-        if(category === 'All') {
+        if (category === 'All') {
             setFilteredProjects(projects)
         } else {
             const filtered = projects.filter(project => project.category === category)
@@ -26,52 +27,52 @@ const Portfolio = () => {
     const handleImageClick = (url) => {
         console.log('Image clicked:', url); // Debugging line
         window.open(url, '_blank', 'noopener,noreferrer');
-      };
-  return (
-    <section>
-        <header>
-            <h2 className='h2 article-title'>Portfolio</h2>
-        </header>
-        {/* Filter Buttons */}
-        <ul className='filter-list'>
-            {['All', 'Frontend','Frontend + React','Full Stack (Frontend + Node)'].map(category =>(
-                <li key={category} className='filter-item'>
-                    <button onClick={()=> handleFilterClick(category)} 
-                    data-filter-btn
-                    className={category === selectedCategory ? 'active' : ""}>{category}</button>
-                </li>
-            ))}
-        </ul>
-
-        {/* Show Portfolio */}
-
-        <section className='projects'>
-            <ul className='project-list'>
-                {
-                    filteredProjects.map(project => (
-                        <li key={project.id} className='project-item active'
-                        data-fliter-items
-                        data-category={project.category}
-                        >
-                            <a href="#">
-                                <figure className='project-img'>
-                               { <div className='project-item-icon-box'>
-                                    <FaRegEye onClick={() =>{handleImageClick(project.imageUrl)}}/>
-                                </div>}
-                                    <img src={project.image} alt={project.title} width="350" 
-                                     />
-                                </figure>
-                                <h3 className='project-title'>{project.title}</h3>
-                                <p className='project-category'>{project.category}</p>
-                            </a>
-                        </li>
-                    )) 
-                }
+    };
+    return (
+        <section>
+            <header>
+                <h2 className='h2 article-title'>Portfolio</h2>
+            </header>
+            {/* Filter Buttons */}
+            <ul className='filter-list'>
+                {['All', 'Frontend', 'Frontend + React', 'Full Stack (Frontend + Node)'].map(category => (
+                    <li key={category} className='filter-item'>
+                        <button onClick={() => handleFilterClick(category)}
+                            data-filter-btn
+                            className={category === selectedCategory ? 'active' : ""}>{category}</button>
+                    </li>
+                ))}
             </ul>
 
+            {/* Show Portfolio */}
+
+            <section className='projects'>
+                <ul className='project-list'>
+                    {
+                        filteredProjects.map(project => (
+                            <li key={project.id} className='project-item active'
+                                data-fliter-items
+                                data-category={project.category}
+                            >
+                                <a href="#">
+                                    <figure className='project-img'>
+                                        {<div className='project-item-icon-box'>
+                                            <FaRegEye onClick={() => { handleImageClick(project.imageUrl) }} />
+                                        </div>}
+                                        <img src={project.image} alt={project.title} width="350"
+                                        />
+                                    </figure>
+                                    <h3 className='project-title'>{project.title}</h3>
+                                    <p className='project-category'>{project.category}</p>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+            </section>
         </section>
-    </section>
-  )
+    )
 }
 
 export default Portfolio
